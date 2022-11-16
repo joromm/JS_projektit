8d02d94cbef76c07b90bb9da19f1c6d90622ae3e
joromm <joromm2@gmail.com>
2022-11-16 16:23:05 +0200

    2t9

--- moduuli3/t3/3.js
+++ moduuli3/t3/3.js
@@ -1,2 +1,8 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+
+for (let i of names) {
+  const li = document.createElement('li');
+  li.innerText = i;
+  document.querySelector('#target').appendChild(li)
+}
