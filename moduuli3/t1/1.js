4b3100a2f5ecda418fe5eea127be892764407843
joromm <joromm2@gmail.com>
2022-11-28 16:45:11 +0200

    3t8

--- moduuli3/t1/1.js
+++ moduuli3/t1/1.js
@@ -4,4 +4,4 @@ document.querySelector('#target').innerHTML = '<li>First Item</li>\n' +
     '<li>Second Item</li>\n' +
     '<li>Third Item</li>\n';
 
-document.querySelector('#target').className = "my-class"
+document.querySelector('#target').className = 'my-class';
